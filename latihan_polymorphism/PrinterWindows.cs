8423b9e90f4a94c84e712d779e813526a2341356
salmaaghisn <97302116+salmaaghisn@users.noreply.github.com>
2022-06-11 22:14:33 +0700

    Add files via upload

---
+++ latihan_polymorphism/PrinterWindows.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace latihan_polymorphism
+{
+    class PrinterWindows
+    {
+        public string name { get; set; }
+
+        public virtual void Show()
+        {
+
+        }
+
+        public virtual void Print()
+        {
+
+        }
+    }
+}
---
+++ latihan_polymorphism/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace latihan_polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrinterWindows printer;
+
+            Console.WriteLine("Pilih printer:");
+            Console.WriteLine("1. Epson");
+            Console.WriteLine("2. Canon");
+            Console.WriteLine("3. LaserJet\n");
+
+            Console.Write("Nomor printer [1..3]: ");
+            int nomorPrinter = Convert.ToInt32(Console.ReadLine());
+
+            if (nomorPrinter == 1)
+            {
+                printer = new Epson();
+            }
+            else if (nomorPrinter == 2)
+            {
+                printer = new Canon();
+            }
+            else
+            {
+                printer = new LaserJet();
+            }
+
+            printer.Show();
+            printer.Print();
+
+            Console.ReadKey();
+        }
+    }
+}
